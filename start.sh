99a1093d56d9cc1800db94cb64a0ff7a988451d1
saurabh8753 <130946713+saurabh8753@users.noreply.github.com>
2023-09-07 19:36:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  Advance-Auto-Filter/Advance-Auto-Filter /Advance-Auto-Filter
+  Advance-Auto-Filters/Advance-Auto-Filters /Advance-Auto-Filters
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-Auto-Filter
+  git clone $UPSTREAM_REPO /Advance-Auto-Filters
 fi
-cd /Advance-Auto-Filter
+cd /Advance-Auto-Filters
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
